2dad255747da18088a3ee6ba39d7734c84e65971
gabipfeffer <gabrielpfeffer@gmail.com>
2023-06-05 15:22:00 -0300

    feat: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+
+export default function Header() {
+  return (
+    <header>
+      {/* TOP NAV*/}
+      <div className={"flex items-center bg-amazon_blue flex-grow px-1 py-2"}>
+        <div className={"mt-2 flex items-center flex-grow sm:flex-grow-0"}>
+          <Image
+            src={"https://links.papareact.com/f90"}
+            alt={"Logo"}
+            width={110}
+            height={40}
+            style={{ objectFit: "contain" }}
+            className={"cursor-pointer"}
+          />
+        </div>
+
+        <div
+          className={
+            "bg-yellow-400 hover:bg-yellow-500 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer"
+          }
+        >
+          <input
+            type={"text"}
+            className={
+              "p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            }
+          />
+          <MagnifyingGlassIcon className={"h-12 p-4"} />
+        </div>
+
+        {/*  RIGHT*/}
+        <div
+          className={
+            "text-white flex flex-row items-center text-xs space-x-6 mx-6 whitespace-nowrap"
+          }
+        >
+          <div className={"link"}>
+            <p>Hello Gabi Pfeffer</p>
+            <p className={"font-extrabold md:text-sm"}>Account & Lists</p>
+          </div>
+          <div className={"link"}>
+            <p>Returns</p>
+            <p className={"font-extrabold md:text-sm"}>& Orders</p>
+          </div>
+          <div className={"relative link flex items-center"}>
+            <span
+              className={
+                "absolute top-0 right-0 md:right-7 h-4 w-4 bg-yellow-400 rounded-full text-black font-bold text-center"
+              }
+            >
+              0
+            </span>
+            <ShoppingCartIcon className={"h-10"} />
+            <p className={"font-extrabold md:text-sm hidden md:inline mt-2"}>
+              Cart
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*  BOTTOM NAV*/}
+      <div
+        className={
+          "flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm"
+        }
+      >
+        <p className={"link flex items-center"}>
+          <Bars3Icon className={"h-6 mr-1"} /> All
+        </p>
+        <p className={"link"}>Prime Video</p>
+        <p className={"link"}>Amazon Business</p>
+        <p className={"link"}>Today&rsquo;s Deals</p>
+        <p className={"link hidden lg:inline-flex"}>Electronics</p>
+        <p className={"link hidden lg:inline-flex"}>Food & Grocery</p>
+        <p className={"link hidden lg:inline-flex"}>Prime</p>
+        <p className={"link hidden lg:inline-flex"}>Buy Again</p>
+        <p className={"link hidden lg:inline-flex"}>Shopper Toolkit</p>
+        <p className={"link hidden lg:inline-flex"}>Health & Personal Care</p>
+      </div>
+    </header>
+  );
+}
